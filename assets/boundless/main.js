52c583bce4c49a00b5954aa6aa38f68f65fd7750
abasalilov <abasalilov@gmail.com>
2019-01-04 00:16:55 -0800

    simple

--- assets/boundless/main.js
+++ assets/boundless/main.js
@@ -1,24 +1,24 @@
-window.onload = function(e) {
-    var contactForm = document.getElementById('contact'); 
-    var contactBtn = document.getElementById('fixed-contact');
-    var contactCloseBtn = document.getElementById('contact-close-btn');
+// window.onload = function(e) {
+//     var contactForm = document.getElementById('contact'); 
+//     var contactBtn = document.getElementById('fixed-contact');
+//     var contactCloseBtn = document.getElementById('contact-close-btn');
 
-    var timeoutId = setTimeout(function() {
-        contactBtn.classList.add('shake');
-    }, 10000);
+//     var timeoutId = setTimeout(function() {
+//         contactBtn.classList.add('shake');
+//     }, 10000);
 
-    contactBtn.onclick = function(e) {
-        e.preventDefault();
+//     contactBtn.onclick = function(e) {
+//         e.preventDefault();
 
-        clearTimeout(timeoutId);
-        contactForm.classList.add('active');
-    }
+//         clearTimeout(timeoutId);
+//         contactForm.classList.add('active');
+//     }
 
-    contactCloseBtn.onclick = function(e) {
-        e.preventDefault();
+//     contactCloseBtn.onclick = function(e) {
+//         e.preventDefault();
 
-        contactForm.classList.remove('active');
-    }
-}
+//         contactForm.classList.remove('active');
+//     }
+// }
 
-setTimeout(function() { document.body.classList.add(window.location.pathname.replace('/', '')) }, 0);
+// setTimeout(function() { document.body.classList.add(window.location.pathname.replace('/', '')) }, 0);
