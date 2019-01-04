f8825a1a2d2381fcd70a77185e0c644774e1c6af
Aleksandr Basalilov <abasalilov@gmail.com>
2019-01-04 00:20:38 -0800

    Updated Welcome To Jekyll and 51 other files

---
+++ assets/boundless/main.js
@@ -0,0 +1,24 @@
+// window.onload = function(e) {
+//     var contactForm = document.getElementById('contact'); 
+//     var contactBtn = document.getElementById('fixed-contact');
+//     var contactCloseBtn = document.getElementById('contact-close-btn');
+
+//     var timeoutId = setTimeout(function() {
+//         contactBtn.classList.add('shake');
+//     }, 10000);
+
+//     contactBtn.onclick = function(e) {
+//         e.preventDefault();
+
+//         clearTimeout(timeoutId);
+//         contactForm.classList.add('active');
+//     }
+
+//     contactCloseBtn.onclick = function(e) {
+//         e.preventDefault();
+
+//         contactForm.classList.remove('active');
+//     }
+// }
+
+// setTimeout(function() { document.body.classList.add(window.location.pathname.replace('/', '')) }, 0);
