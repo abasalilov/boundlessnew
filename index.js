f92ca235d9a977be134b47977443de0f3280d6ef
abasalilov <abasalilov@gmail.com>
2019-01-24 16:48:26 -0800

    udpated

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require("express"),
+  path = require("path"),
+  app = express(),
+  // getting port this way
+  port = process.env.PORT || process.argv[2] || 8080;
+
+// using app.use to use static files in my public
+// folder for the root level of the site
+app.use("/", express.static("public"));
+
+app.listen(port, function() {
+  console.log("app up on port: " + port);
+});
